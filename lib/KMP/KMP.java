e74c9a5d1f00d2a805f59af2c6f76fb1385fd78f
davechai <davecqx@gmail.com>
2020-04-12 16:18:00 +0800

    动态规划之KMP字符匹配算法

---
+++ lib/KMP/KMP.java
@@ -0,0 +1,47 @@
+public class Code {
+  public static void main(String[] args) {
+    KMP kmp = new KMP("aaab");
+    int pos1 = kmp.search("aaacaab"); //4
+    int pos2 = kmp.search("aaaaaaab"); //4
+    System.out.println(pos1);
+  }
+}
+
+class KMP {
+    private int[][] dp;
+    private String pat;
+
+    public KMP(String pat) {
+        this.pat = pat;
+        int M = pat.length();
+        // dp[状态][字符] = 下个状态
+        dp = new int[M][256];
+        // base case
+        dp[0][pat.charAt(0)] = 1;
+        // 影子状态 X 初始为 0
+        int X = 0;
+        // 构建状态转移图（稍改的更紧凑了）
+        for (int j = 1; j < M; j++) {
+            for (int c = 0; c < 256; c++)
+                dp[j][c] = dp[X][c];
+            dp[j][pat.charAt(j)] = j + 1;
+            // 更新影子状态
+            X = dp[X][pat.charAt(j)];
+        }
+    }
+
+    public int search(String txt) {
+        int M = pat.length();
+        int N = txt.length();
+        // pat 的初始态为 0
+        int j = 0;
+        for (int i = 0; i < N; i++) {
+            // 计算 pat 的下一个状态
+            j = dp[j][txt.charAt(i)];
+            // 到达终止态，返回结果
+            if (j == M) return i - M + 1;
+        }
+        // 没到达终止态，匹配失败
+        return -1;
+    }
+}
